40d4e13e49ff0496e9779167f83b81666c66b2c0
ShadowNinja1210 <mohitjeswani1210@gmail.com>
2025-03-07 00:27:59 +0530

    Changed the products page from server side to fetch the products data on client and render on client side only. Added the favicon.

--- app/(routes)/(main)/layout.tsx
+++ app/(routes)/(main)/layout.tsx
@@ -7,6 +7,7 @@ import Footer from "@/components/footer";
 import { usePathname } from "next/navigation";
 import { ContactFloater } from "@/components/contact/contacts-floater";
 import { BackToTop } from "@/components/back-to-top";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -17,6 +18,14 @@ export default function RootLayout({
 
   return (
     <Providers>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Earthycrafts" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <Navbar className={path === "/products" ? "sticky top-0" : ""} />
       <ContactFloater />
       {children}
--- app/(routes)/(main)/products/page.tsx
+++ app/(routes)/(main)/products/page.tsx
@@ -1,50 +1,53 @@
+"use client";
+
 import SpinLoader from "@/components/loaders/spin-loader";
 import ProductsLayout from "@/components/products/products-layout";
+import { fetchAllProducts } from "@/lib/api";
 import { INewProduct } from "@/lib/schema";
+import { useQuery } from "@tanstack/react-query";
 import { lowerCase } from "lodash";
 import { Suspense } from "react";
 
-export default async function AllProductsPage() {
-  let products: INewProduct[] = [];
-
-  try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/product`);
-    products = await response.json();
-  } catch (error) {
-    console.error("Error fetching products:", error);
-  }
+export default function AllProductsPage() {
+  const { data: products, isLoading } = useQuery<INewProduct[]>({
+    queryKey: ["products"],
+    queryFn: fetchAllProducts,
+  });
 
   // Create category mapping
   const categoryMap = new Map<string, { subCategories: Set<string>; type: string }>();
-  products.forEach((product) => {
-    const { primaryCategory, subCategory, secondaryCategory } = product;
 
-    // Handle primary categories
-    primaryCategory?.forEach((category) => {
-      const normalizedCategory = lowerCase(category.trim());
+  if (!isLoading) {
+    products?.forEach((product) => {
+      const { primaryCategory, subCategory, secondaryCategory } = product;
 
-      if (!categoryMap.has(normalizedCategory)) {
-        categoryMap.set(normalizedCategory, {
-          subCategories: new Set(),
-          type: secondaryCategory?.includes(category) ? "secondary" : "primary",
-        });
-      }
+      // Handle primary categories
+      primaryCategory?.forEach((category) => {
+        const normalizedCategory = lowerCase(category.trim());
 
-      // Add subCategory if it exists
-      if (subCategory) {
-        categoryMap.get(normalizedCategory)?.subCategories.add(lowerCase(subCategory.trim()));
-      }
+        if (!categoryMap.has(normalizedCategory)) {
+          categoryMap.set(normalizedCategory, {
+            subCategories: new Set(),
+            type: secondaryCategory?.includes(category) ? "secondary" : "primary",
+          });
+        }
+
+        // Add subCategory if it exists
+        if (subCategory) {
+          categoryMap.get(normalizedCategory)?.subCategories.add(lowerCase(subCategory.trim()));
+        }
+      });
+
+      // Handle secondary categories
+      secondaryCategory?.forEach((category) => {
+        const normalizedCategory = lowerCase(category.trim());
+
+        if (!categoryMap.has(normalizedCategory)) {
+          categoryMap.set(normalizedCategory, { subCategories: new Set(), type: "secondary" });
+        }
+      });
     });
-
-    // Handle secondary categories
-    secondaryCategory?.forEach((category) => {
-      const normalizedCategory = lowerCase(category.trim());
-
-      if (!categoryMap.has(normalizedCategory)) {
-        categoryMap.set(normalizedCategory, { subCategories: new Set(), type: "secondary" });
-      }
-    });
-  });
+  }
 
   // Convert Map to Array
   const categories = Array.from(categoryMap, ([name, { subCategories, type }]) => ({
@@ -54,8 +57,12 @@ export default async function AllProductsPage() {
   }));
 
   return (
-    <Suspense fallback={<SpinLoader size="xl" />}>
-      <ProductsLayout categories={categories} products={products} />;
+    <Suspense fallback={isLoading && <SpinLoader size="xl" />}>
+      {isLoading ? (
+        <SpinLoader size="xl" />
+      ) : (
+        <ProductsLayout categories={categories} products={products as INewProduct[]} />
+      )}
     </Suspense>
   );
 }
